92308e57077b1e1c287ca75221e4faf75969c2a3
Andrew McDole <mcdoleandrew@gmail.com>
2018-06-04 08:04:24 -0500

    Clients able to connect to the tcp server

--- client.py
+++ client.py
@@ -1,11 +1,32 @@
 #!/usr/local/bin/python3
 
 import socket
+import sys
 
-s = socket.socket()
-host = "10.122.130.230"
-port = 6789
+# s = socket.socket()
+# host = "127.0.0.1"
+# port = 6789
+#
+# s.connect((host, port))
+# print ("Successfully Connected to Server")
+# # print (s.recv(1024).decode())
+# s.close()
 
-s.connect((host, port))
-print (s.recv(1024).decode())
-s.close()
+HOST, PORT = "localhost", 6789
+data = " ".join(sys.argv[1:])
+
+# Create a socket to server and send data
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to the server and send data
+    sock.connect((HOST, PORT))
+    sock.send((data + "\n").encode())
+
+    # Receive data form the server and shut down
+    received = sock.recv(1024)
+finally:
+    sock.close()
+
+print ("Sent:    {}".format(data))
+print ("Received:    {}".format(received))
--- requestHandler.py
+++ requestHandler.py
@@ -3,8 +3,28 @@
 import socketserver
 
 class RequestHandler(socketserver.BaseRequestHandler):
-    def __init__(self):
-        print ("Created request handler")
+    """
+    The RequestHandler is instantiated once per connection
+    to the server, and must override the handle() method
+    to implement communication to the client
+    """
 
     def handle(self):
         print("Request Received")
+        # self.data = self.request.recv(1024).strip()
+        # print "{} wrote:".format(self.client_address[0])
+        # print self.data.decode()
+        self.request.send("Thank you for connecting".encode())
+
+    """
+    if __name__ == "__main__" is a way for this file
+    to be run as the main program
+    """
+if __name__ == "__main__":
+    HOST, PORT = "", 6789
+
+    #create a server, binding it to localhost on port 6789
+    server = socketserver.TCPServer((HOST, PORT), RequestHandler)
+
+    #Activate the server; this will keep running until you interrupt
+    server.serve_forever()
