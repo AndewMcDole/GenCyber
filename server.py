424ebe6f072f06f3f82cd84b56b9bf056748fd13
Andrew McDole <mcdoleandrew@gmail.com>
2018-06-06 09:05:40 -0500

    Added the ability for clients to connect to server and add themselves to directory

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 #!/usr/local/bin/python3
 
 import socketserver
+import ClientDirectory
+
+CD = ClientDirectory.ClientDirectory(2)
 
 class AvengerHandler(socketserver.BaseRequestHandler):
     """
@@ -12,8 +15,16 @@ class AvengerHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # print("Request Received")
         self.data = self.request.recv(1024).strip()
-        print ("{} wrote: {}".format(self.client_address[0], self.data.decode()))
-        self.request.send("Thank you for connecting to the Avenger's Stone Hunt".encode())
+        message = self.data.decode()
+        print ("{} wrote: {}".format(self.client_address[0], message))
+        #Check the first character
+        if message[0] == "0":
+            name = message[2:]
+            print ("Adding new client {} to client directory".format(name))
+            CD.addClient(name, self.client_address[0])
+            self.request.send("Thank you for connecting to the Avenger's Stone Hunt".encode())
+        else:
+            print ("Transmitting message")
 
 
 HOST, PORT = "", 6789
