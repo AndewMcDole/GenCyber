b60a7767f885503c7810bfff79b7d18d54255ab3
jrvick42 <jrvick42@students.tntech.edu>
2018-06-28 10:29:17 -0500

    testing

--- client.py
+++ client.py
@@ -94,8 +94,11 @@ def send():
 def receive(sock):
     print ("client: receive")
     new_message = socks.recv(2048)
-    checkForConnectionLoss(new_message)
 
+    if new_message.decode() == 'success':
+        new_message = socks.recv(2048)
+        
+    checkForConnectionLoss(new_message)
     new_message = new_message.decode()
 
     # Once we receive a message, we need to strip off the name and winnow the message
